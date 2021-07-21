ec423c56c4207dbb64937b00fd7914b54e21fffb
Vladimir T <tvvladimir@icloud.com>
2021-07-21 15:44:37 +0200

    1

---
+++ modules/pygame.py
@@ -0,0 +1,8 @@
+# Documentation at:
+# https://www.pygame.org/wiki/GettingStarted
+
+# Install
+# python3 -m pip install -U pygame --user
+# python3 -m pip install -U pygame --user # works for me
+# See if it works:
+# python3 -m pygame.examples.aliens
