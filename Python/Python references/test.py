84550b9a274beaa8ce7eff29c17b3eac9f10ee46
Grigory747 <grigory.tazev@gmail.com>
2022-12-11 07:55:31 +0100

    dfg
    dfg

--- Python/Python references/test.py
+++ Python/Python references/test.py
@@ -1,46 +1,9 @@
-import abc
+x = 1                             # input size
+given_array = [*range(1, x, 1)]   # generate list
 
-# Abstract cl ass
-class GetSetParent(object):
-  __masterclass__ = abc.ABCMeta
-  def __init__(self, value):
-    self.val = 0
-  def set_val(self, value):
-    self.val = value
-  def get_val(self):
-    return self.val
-  @abc.abstractmethod
-  def showdoc(self):
-    return
+total = 0
+for i in given_array:
+    total += 1
+    print(total)
 
-class GetSetInt(GetSetParent):
-  def set_val(self, value):
-    if not isinstance(value, int):
-      value = 0
-    super(GetSetInt, self).set_val(value)
-  def showdoc(self):
-    print('GetSetInt object ({0}), only accepts integer values'.format(id(self)))
-
-class GetSetList(GetSetParent):
-  def __init__(self, value=0):
-    self.vallist = [value]
-  def get_val(self):
-    return self.vallist[-1]
-  def get_vals(self):
-    return self.vallist
-  def set_val(self, value):
-    self.vallist.append(value)
-  def showdoc(self):
-    print('GetSetList object, len{0}, stores history of values set'.format(len(self.vallist)))
-
-x = GetSetInt(3)
-x.set_val(5)
-print(x.get_val())
-x.showdoc()
-
-gsl = GetSetList(5)
-gsl.set_val(10)
-gsl.set_val(20)
-print(gsl.get_val())
-print(gsl.get_vals())
-gsl.showdoc()
+print(total) 
